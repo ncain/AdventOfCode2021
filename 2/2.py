1219f525371c1fdfd35416f878d9ed9dd932784d
Noah <noah@cain.email>
2021-12-02 14:08:21 -0700

    Failed to stage changes, I think?

--- 2/2.py
+++ 2/2.py
@@ -1,16 +1,33 @@
 with open('input.txt') as input:
   lines = [line.rstrip() for line in input.readlines()]
 
-horizontal = 0
-depth = 0
-for line in lines:
-  direction, amount = line.split(' ')
-  if direction == "forward":
-    horizontal += int(amount)
-  elif direction == "down":
-    depth += int(amount)
-  elif direction == "up":
-    depth -= int(amount)
+def naive_course():
+  position = 0
+  depth = 0
+  for line in lines:
+    direction, amount = line.split(' ')
+    if direction == "forward":
+      position += int(amount)
+    elif direction == "down":
+      depth += int(amount)
+    elif direction == "up":
+      depth -= int(amount)
+  return position * depth
 
-print(horizontal * depth)
+def clever_course():
+  aim = 0
+  position = 0
+  depth = 0
+  for line in lines:
+    direction, amount = line.split(' ')
+    if direction == "forward":
+      position += int(amount)
+      depth += (aim * int(amount))
+    elif direction == "down":
+      aim += int(amount)
+    elif direction == "up":
+      aim -= int(amount)
+  return position * depth
 
+print("Part one: " + str(naive_course()))
+print("Part two: " + str(clever_course()))
