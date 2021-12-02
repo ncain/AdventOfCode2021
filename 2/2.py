42f872a79b66cf5e9d777463f71f14d9d11cceb5
Noah <noah@cain.email>
2021-12-02 15:16:04 -0700

    Consolidated the repeated code in the day 2 solution.

--- 2/2.py
+++ 2/2.py
@@ -1,33 +1,28 @@
 with open('input.txt') as input:
   lines = [line.rstrip() for line in input.readlines()]
 
-def naive_course():
+def chart_course(naive = False):
+  '''
+  Compute the submarine's distance; if naive is true, aim is ignored and "up" and "down" move the submarine directly.
+  '''
   position = 0
   depth = 0
-  for line in lines:
-    direction, amount = line.split(' ')
-    if direction == "forward":
-      position += int(amount)
-    elif direction == "down":
-      depth += int(amount)
-    elif direction == "up":
-      depth -= int(amount)
-  return position * depth
-
-def clever_course():
   aim = 0
-  position = 0
-  depth = 0
   for line in lines:
     direction, amount = line.split(' ')
+    amount = int(amount)
     if direction == "forward":
-      position += int(amount)
-      depth += (aim * int(amount))
+      position += amount
+      if not naive:
+        depth += aim * amount
     elif direction == "down":
-      aim += int(amount)
+      aim += amount
     elif direction == "up":
-      aim -= int(amount)
+      aim -= amount
+  if naive:
+    depth = aim
   return position * depth
 
-print("Part one: " + str(naive_course()))
-print("Part two: " + str(clever_course()))
+
+print("Part one: " + str(chart_course(naive=True)))
+print("Part two: " + str(chart_course()))
